c0a032dd1c5618e4656a5a563e9a5d06e4c3ee74
psmzakaria <psmzakaria@gmail.com>
2018-07-05 15:45:41 +0800

    ADD3

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require("./app");
-app.listen(process.env.PORT || 3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log(`Your app has started on port${PORT}...`);
   });
   
